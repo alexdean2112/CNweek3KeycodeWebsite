c4279b0042f60c580793fe7431b9e3ec6f4523ba
alexdean2112 <alexdeanofficial2112@gmail.com>
2022-09-12 16:12:00 +0100

    added media query for phone compatability

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const infobar = document.getElementById("infobar")
 const infobar2 = document.getElementById("infobar2")
 const h1 = document.getElementsByTagName("h1")
+const h3 = document.getElementsByTagName("h3")
 const p = document.getElementsByTagName("p")
 
 
@@ -10,6 +11,9 @@ document.addEventListener("keypress", (event) => {
     h1[0].textContent = "charCode" + (event.charCode)
     h1[0].style.fontSize = "150px"
     h1[0].style.marginTop = "30vh"
+    h3[0].textContent = "charCode" + (event.charCode)
+    h3[0].style.fontSize = "50px"
+    h3[0].style.marginTop = "30vh"
     p[0].textContent = (event.key)
     p[1].textContent = (event.code)
     p[2].textContent = (event.which)
