a0394c369511df5d708f4731fe2c4cc9cd5f44fd
alexdean2112 <alexdeanofficial2112@gmail.com>
2022-09-12 15:08:22 +0100

    first draft

---
+++ java.js
@@ -0,0 +1,17 @@
+const infobar = document.getElementById("infobar")
+const infobar2 = document.getElementById("infobar2")
+const h1 = document.getElementsByTagName("h1")
+const p = document.getElementsByTagName("p")
+
+
+document.addEventListener("keypress", (event) => {
+    infobar.style.display = "flex"
+    infobar2.style.display = "flex"
+    h1[0].textContent = (event.charCode)
+    h1[0].style.fontSize = "150px"
+    h1[0].style.marginTop = "30vh"
+    p[0].textContent = (event.key)
+    p[1].textContent = (event.code)
+    p[2].textContent = (event.which)
+})
+
