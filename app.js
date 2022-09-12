270246249fff03958988fe12cd8ee5ac281ad7d1
alexdean2112 <alexdeanofficial2112@gmail.com>
2022-09-12 16:54:18 +0100

    edited java and html

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ document.addEventListener("keypress", (event) => {
     infobar.style.display = "flex"
     infobar2.style.display = "flex"
     h1[0].textContent = "charCode" + (event.charCode)
-    h1[0].style.fontSize = "150px"
-    h1[0].style.marginTop = "30vh"
+
+
     h3[0].textContent = "charCode" + (event.charCode)
-    h3[0].style.marginTop = "30vh"
+
     p[0].textContent = (event.key)
     p[1].textContent = (event.code)
     p[2].textContent = (event.which)
